0001b626c9e935d804b4137892d52c8a4635f593
Andrew Junzki <andrew.junzki@gmail.com>
2023-06-05 00:34:52 +0800

    fix(setup): add aarch64 geckodriver

---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+
+ARCH=$(uname -m)
+
+install_geckodriver() {
+  gecko_driver="misc/geckodriver-v0.33.0-linux64.tar.gz"
+  if [[ ${ARCH} -eq "aarch64" ]]; then
+    gecko_driver="misc/geckodriver-v0.33.0-linux-aarch64.tar.gz"
+  fi
+
+  tar -zxvf "${gecko_driver}" /usr/local/bin
+}
+
+
+collect_static() {
+  python manage.py collectstatic --noinput
+}
+
+cleanup() {
+  rm -rfv misc/
+}
+
+install_geckodriver
+collect_static
+
+
+cleanup
